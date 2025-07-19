af9734e908bcc175d1ac50576d29936ce512e38e
mawed22 <foupouamohamed3@gmail.com>
2025-07-19 16:50:28 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 IMAGE=$1
-CONTAINER_NAME=web
+#CONTAINER_NAME=Test
 
 # Stop previous container
-docker rm -f $CONTAINER_NAME
+#docker rm -f $CONTAINER_NAME
 
 # Pull and run
 docker pull $IMAGE
-docker run -d --name $CONTAINER_NAME -p 80:80 $IMAGE
+#docker run -d --name $CONTAINER_NAME -p 80:80 $IMAGE
+docker run -d -p 80:80 $IMAGE
